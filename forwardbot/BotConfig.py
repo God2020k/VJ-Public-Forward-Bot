e74ccf7cd6598b371f738e06b783c49569133784
God2020k <107332649+God2020k@users.noreply.github.com>
2024-11-18 16:16:19 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = environ.get("API_ID", "20204401")
-    API_HASH = environ.get("API_HASH", "6ec374e7e7492de06a9b22bb746bd20f")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6922036916:AAHod6WEiUf70eq6ytG2P1uRrk6__uQ-xHU")
-    STRING_SESSION = environ.get("STRING_SESSION", "")
-    SUDO_USERS = environ.get("SUDO_USERS", "6168162777 6366990600")
+    API_ID = environ.get("API_ID", "17627887")
+    API_HASH = environ.get("API_HASH", "d14e54b0791ca1f8b0f26786439e336e")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7144831299:AAFAG5xpXE2PHo2USbtIP2TL8CwaG6U7sAI")
+    STRING_SESSION = environ.get("STRING_SESSION", "BQEXr9MAcnl2a6ZgDoCiRjomra8J1AaCPX8uPEif_20J9Y29at89M7eX4PVT4gnaEC1QGH_f3xicSAvLxJ8tTrfmIYF0HvOeLDRqcV0iImJwSHQRs7fOCiS7yLLMGC5qdRlr-X7FG7QdEuT9HBq4jyo73JsrKFmgwOhg7MwesBj1RflC3XwyU0SvS7NK565VE9U-AWRj6VFUI_tCtSoDxaI5JaipOK_PGR_SlK1VL6YacuoECEHh6adNGK1aOINxuyOAKGbyl40mIbrhpUv9f1dflg_n_f8MrhDNwljP_b_GTNPAXpXqqfDvh5Zf-P0HTK8LamVl7s9WWMz0aNN67wAbHUnYQwAAAABW22yGAA")
+    SUDO_USERS = environ.get("SUDO_USERS", "5211097531")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
